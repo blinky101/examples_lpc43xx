11e8987e6f9d60ee37da18ee282f4f3bee41054a
Sjors de Wit <dev@sjorsdewit.name>
2019-07-30 18:23:55 +0200

    SDCard benchmark: save all timing
    A separate meta file is saved per test containing the write times (in
    ms) for each write performed. This enables more advanced analysis
    afterwards.

--- sdcard/src/main.c
+++ sdcard/src/main.c
@@ -32,11 +32,21 @@ const GPIO *led_warn;
 #define TEST_2
 #define TEST_3
 #define TEST_4
+#define TEST_5
+
+// Enable to write a file with all latency numbers of each test.
+// NOTE: writing this file may take even longer than the tests themselves...
+#define TIMES_TRACE
 
 void SysTick_Handler(void)
 {
     GPIO_HAL_toggle(led_blue);
 }
+
+
+static uint8_t g_buffer[32*1024];
+static uint16_t g_write_times[16*1024]  __attribute__((section(".bss.$extra_bss")));
+
 
 static void error(void)
 {
@@ -44,7 +54,52 @@ static void error(void)
     while(1);
 }
 
+static void prepare_test(void)
+{
+    // prepare buffer: dummy data to write to sdcard file
+    for(size_t n=0;n<sizeof(g_buffer);n++) {
+        g_buffer[n] = (n & 0xFF);
+    }
+    memset(g_write_times, 0, sizeof(g_write_times));
+
+}
+
+static void write_meta_files(unsigned int test_id,
+        unsigned int total_time, unsigned int max_latency,
+        uint16_t *write_times, size_t num_write_times)
+{
+    // Append to the meta file with the test result summaries
+    char result_str[256];
+    snprintf(result_str, sizeof(result_str), "%d: max=%u ms, total=%u ms\n",
+            test_id, max_latency, total_time);
+    sdcard_write_to_file("results.txt", result_str, strlen(result_str));
+
+#ifdef TIMES_TRACE
+    // Write the timing numbers to a file (one number per line)
+    char fname[16];
+    snprintf(fname, sizeof(fname), "times-%d.csv", test_id);
+
+    // assume iterations is a multiple of 8 for a bit more speed of this
+    // naive implementation..
+    for(size_t i=0;i<num_write_times;i+=8) {
+        snprintf(result_str, sizeof(result_str), "%u\n%u\n%u\n%u\n%u\n%u\n%u\n%u\n",
+                (unsigned int)write_times[i],
+                (unsigned int)write_times[i+1],
+                (unsigned int)write_times[i+2],
+                (unsigned int)write_times[i+3],
+                (unsigned int)write_times[i+4],
+                (unsigned int)write_times[i+5],
+                (unsigned int)write_times[i+6],
+                (unsigned int)write_times[i+7]);
+        sdcard_write_to_file(fname, result_str, strlen(result_str));
+    }
+#endif
+}
+
+
+
 int main(void) {
+    memset(g_write_times, 0, sizeof(g_write_times));
     // board-specific setup
     board_setup();
     board_setup_NVIC();
@@ -83,81 +138,80 @@ int main(void) {
 
 #ifdef TEST_1
     {
-        // buffer: dummy data to write to sdcard file
-        uint8_t buffer[512];
-        for(size_t n=0;n<sizeof(buffer);n++) {
-            buffer[n] = (n & 0xFF);
-        }
+        const size_t sizeof_buffer = 512;
+        const size_t iterations = 16*1024;
+
+        prepare_test();
 
         // Perform write test in small blocks of data.
         // The file is opened and closed for each block...
         uint64_t max_latency = 0;
         const uint64_t t_start = delay_get_timestamp();
 
-        for(size_t i=0;i<16*1024;i++) {
-            const size_t offset = i * sizeof(buffer);
+        for(size_t i=0;i<iterations;i++) {
+            const size_t offset = i * sizeof_buffer;
 
             const uint64_t t_pre = delay_get_timestamp();
-            sdcard_write_to_file_offset("perf-1.bin", (char*)buffer, sizeof(buffer), offset);
+            sdcard_write_to_file_offset("perf-1.bin", (char*)g_buffer, sizeof_buffer, offset);
             const uint64_t t_post = delay_get_timestamp();
 
+            // Calculate latency, saturate to UINT16_MAX to avoid overflow
+            uint32_t latency = (delay_calc_time_us(t_pre, t_post) / 1000);
+            latency = min(latency, UINT16_MAX);
+            g_write_times[i] = latency;
+
             // Keep track of maximum time spent in one write
-            max_latency = max(max_latency, delay_calc_time_us(t_pre, t_post));
+            max_latency = max(max_latency, latency);
         }
 
         const uint64_t t_end = delay_get_timestamp();
-        const uint64_t total_time = delay_calc_time_us(t_start, t_end);
+        const uint64_t total_time = delay_calc_time_us(t_start, t_end) / 1000;
 
-        // Write a meta file with the test results
-        char result_str[256];
-        snprintf(result_str, sizeof(result_str), "1: max=%u ms, total=%u ms\n",
-                (unsigned int)max_latency / 1000,
-                (unsigned int)total_time / 1000);
-        sdcard_write_to_file("results.txt", result_str, strlen(result_str));
+        write_meta_files(1, total_time, max_latency, g_write_times, iterations);
     }
 #endif
 #ifdef TEST_2
     {
         // buffer: dummy data to write to sdcard file
-        uint8_t buffer[2048];
-        for(size_t n=0;n<sizeof(buffer);n++) {
-            buffer[n] = (n & 0xFF);
-        }
+        const size_t sizeof_buffer = 2048;
+        const size_t iterations = 16*1024;
+
+        prepare_test();
 
         // Perform write test in small blocks of data
         // The file is opened and closed for each block...
         uint64_t max_latency = 0;
         const uint64_t t_start = delay_get_timestamp();
 
-        for(size_t i=0;i<16*1024;i++) {
-            const size_t offset = i * sizeof(buffer);
+        for(size_t i=0;i<iterations;i++) {
+            const size_t offset = i * sizeof_buffer;
 
             const uint64_t t_pre = delay_get_timestamp();
-            sdcard_write_to_file_offset("perf-2.bin", (char*)buffer, sizeof(buffer), offset);
+            sdcard_write_to_file_offset("perf-2.bin", (char*)g_buffer, sizeof_buffer, offset);
             const uint64_t t_post = delay_get_timestamp();
 
+            // Calculate latency, saturate to UINT16_MAX to avoid overflow
+            uint32_t latency = (delay_calc_time_us(t_pre, t_post) / 1000);
+            latency = min(latency, UINT16_MAX);
+            g_write_times[i] = latency;
+
             // Keep track of maximum time spent in one write
-            max_latency = max(max_latency, delay_calc_time_us(t_pre, t_post));
+            max_latency = max(max_latency, latency);
         }
 
         const uint64_t t_end = delay_get_timestamp();
-        const uint64_t total_time = delay_calc_time_us(t_start, t_end);
+        const uint64_t total_time = delay_calc_time_us(t_start, t_end) / 1000;
 
-        // Write a meta file with the test results
-        char result_str[256];
-        snprintf(result_str, sizeof(result_str), "2: max=%u ms, total=%u ms\n",
-                (unsigned int)max_latency / 1000,
-                (unsigned int)total_time / 1000);
-        sdcard_write_to_file("results.txt", result_str, strlen(result_str));
+        write_meta_files(2, total_time, max_latency, g_write_times, iterations);
     }
 #endif
 #ifdef TEST_3
     {
-        // buffer: dummy data to write to sdcard file
-        uint8_t buffer[512];
-        for(size_t n=0;n<sizeof(buffer);n++) {
-            buffer[n] = (n & 0xFF);
-        }
+        // g_buffer: dummy data to write to sdcard file
+        const size_t sizeof_buffer = 512;
+        const size_t iterations = 16*1024;
+
+        prepare_test();
 
         // Perform write test in small blocks of data.
         // File is opened, data is written in blocks, file is closed
@@ -170,19 +224,24 @@ int main(void) {
             error();
         }
 
-        for(size_t i=0;i<16*1024;i++) {
+        for(size_t i=0;i<iterations;i++) {
             const uint64_t t_pre = delay_get_timestamp();
 
             // Write a block of data to file
             UINT bw;
-            if(FR_OK != f_write(&file, buffer, sizeof(buffer), &bw)) {
+            if(FR_OK != f_write(&file, g_buffer, sizeof_buffer, &bw)) {
                 error();
             }
 
             const uint64_t t_post = delay_get_timestamp();
 
+            // Calculate latency, saturate to UINT16_MAX to avoid overflow
+            uint32_t latency = (delay_calc_time_us(t_pre, t_post) / 1000);
+            latency = min(latency, UINT16_MAX);
+            g_write_times[i] = latency;
+
             // Keep track of maximum time spent in one write
-            max_latency = max(max_latency, delay_calc_time_us(t_pre, t_post));
+            max_latency = max(max_latency, latency);
         }
 
         if(FR_OK != f_close(&file)) {
@@ -190,23 +249,18 @@ int main(void) {
         }
 
         const uint64_t t_end = delay_get_timestamp();
-        const uint64_t total_time = delay_calc_time_us(t_start, t_end);
+        const uint64_t total_time = delay_calc_time_us(t_start, t_end) / 1000;
 
-        // Write a meta file with the test results
-        char result_str[256];
-        snprintf(result_str, sizeof(result_str), "3: max=%u ms, total=%u ms\n",
-                (unsigned int)max_latency / 1000,
-                (unsigned int)total_time / 1000);
-        sdcard_write_to_file("results.txt", result_str, strlen(result_str));
+        write_meta_files(3, total_time, max_latency, g_write_times, iterations);
     }
 #endif
 #ifdef TEST_4
     {
-        // buffer: dummy data to write to sdcard file
-        uint8_t buffer[2048];
-        for(size_t n=0;n<sizeof(buffer);n++) {
-            buffer[n] = (n & 0xFF);
-        }
+        // g_buffer: dummy data to write to sdcard file
+        const size_t sizeof_buffer = 2048;
+        const size_t iterations = 16*1024;
+
+        prepare_test();
 
         // Perform write test in small blocks of data.
         // File is opened, data is written in blocks, file is closed
@@ -219,19 +273,24 @@ int main(void) {
             error();
         }
 
-        for(size_t i=0;i<16*1024;i++) {
+        for(size_t i=0;i<iterations;i++) {
             const uint64_t t_pre = delay_get_timestamp();
 
             // Write a block of data to file
             UINT bw;
-            if(FR_OK != f_write(&file, buffer, sizeof(buffer), &bw)) {
+            if(FR_OK != f_write(&file, g_buffer, sizeof_buffer, &bw)) {
                 error();
             }
 
             const uint64_t t_post = delay_get_timestamp();
 
+            // Calculate latency, saturate to UINT16_MAX to avoid overflow
+            uint32_t latency = (delay_calc_time_us(t_pre, t_post) / 1000);
+            latency = min(latency, UINT16_MAX);
+            g_write_times[i] = latency;
+
             // Keep track of maximum time spent in one write
-            max_latency = max(max_latency, delay_calc_time_us(t_pre, t_post));
+            max_latency = max(max_latency, latency);
         }
 
         if(FR_OK != f_close(&file)) {
@@ -239,14 +298,58 @@ int main(void) {
         }
 
         const uint64_t t_end = delay_get_timestamp();
-        const uint64_t total_time = delay_calc_time_us(t_start, t_end);
+        const uint64_t total_time = delay_calc_time_us(t_start, t_end) / 1000;
 
-        // Write a meta file with the test results
-        char result_str[256];
-        snprintf(result_str, sizeof(result_str), "4: max=%u ms, total=%u ms\n",
-                (unsigned int)max_latency / 1000,
-                (unsigned int)total_time / 1000);
-        sdcard_write_to_file("results.txt", result_str, strlen(result_str));
+        write_meta_files(4, total_time, max_latency, g_write_times, iterations);
+    }
+#endif
+#ifdef TEST_5
+    {
+        // g_buffer: dummy data to write to sdcard file
+        const size_t sizeof_buffer = 32*1024;
+        const size_t iterations = 4*1024;
+
+        prepare_test();
+
+        // Perform write test in small blocks of data.
+        // File is opened, data is written in blocks, file is closed
+        uint64_t max_latency = 0;
+        const uint64_t t_start = delay_get_timestamp();
+
+        // Open file
+        FIL file;
+        if(FR_OK != f_open(&file, "perf-5.bin", FA_WRITE | FA_CREATE_ALWAYS)) {
+            error();
+        }
+
+        for(size_t i=0;i<iterations;i++) {
+            const uint64_t t_pre = delay_get_timestamp();
+
+            // Write a block of data to file
+            UINT bw;
+            if(FR_OK != f_write(&file, g_buffer, sizeof_buffer, &bw)) {
+                error();
+            }
+
+            const uint64_t t_post = delay_get_timestamp();
+
+            // Calculate latency, saturate to UINT16_MAX to avoid overflow
+            uint32_t latency = (delay_calc_time_us(t_pre, t_post) / 1000);
+            latency = min(latency, UINT16_MAX);
+            g_write_times[i] = latency;
+
+            // Keep track of maximum time spent in one write
+            max_latency = max(max_latency, latency);
+        }
+
+        if(FR_OK != f_close(&file)) {
+            error();
+        }
+
+        const uint64_t t_end = delay_get_timestamp();
+        const uint64_t total_time = delay_calc_time_us(t_start, t_end) / 1000;
+
+        write_meta_files(5, total_time, max_latency, g_write_times, iterations);
     }
 #endif
 
